e07d761d74fb2de73fd011f30cd3e731c7d78c28
Musharrif_Khan <musharrif777khan@gmail.com>
2026-05-07 00:28:24 +0530

    updated app files

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainSiteLayout from "../layouts/MainSiteLayout";
+import AuthLayout from "../layouts/AuthLayout";
+
+import Home from "../pages/Home";
+import Questionnaire from "../pages/Questionnaire";
+import Results from "../pages/Results";
+import SchemeDetails from "../pages/SchemeDetails";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import SavedSchemes from "../pages/SavedSchemes";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainSiteLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/questionnaire" element={<Questionnaire />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/scheme/:id" element={<SchemeDetails />} />
+          <Route path="/saved-schemes" element={<SavedSchemes />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRoutes;
